516064a8177aec1faf9021be793430c2bd5452a1
Pinaki Brahma <Pinaki.Brahma@gmail.com>
2024-02-25 13:38:54 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='use (local and online) llms alongwith rags for question answering',
+    author='pinaki.brahma',
+    license='MIT',
+)
